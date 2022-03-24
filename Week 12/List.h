f0ec27c05bcd6c2816b5c9db6c0d56bf25f5c843
JMK <zzuhis@gmail.com>
2022-03-24 09:14:11 +0200

    Task 1 completed

--- Week 12/List.h
+++ Week 12/List.h
@@ -6,27 +6,37 @@
 const int max_list = 20;
 
 //Task 1 methods:
-template<class List_entry, class List>
+template<typename List_entry, class List>
 Error_code insert_first(const List_entry &x, List &a_list);
-template<class List_entry, class List>
+
+template<typename List_entry, class List>
 Error_code remove_first(List_entry &x, List &a_list);
-template<class List_entry, class List>
+
+template<typename List_entry, class List>
 Error_code insert_last(const List_entry &x, List &a_list);
-template<class List_entry, class List>
+
+template<typename List_entry, class List>
 Error_code remove_last(List_entry &x, List &a_list);
-template<class List_entry, class List>
+
+template<typename List_entry, class List>
 Error_code median_list(List_entry &x, List &a_list);
-template<class List, class List_entry>
+
+template<class List>
 Error_code interchange(int pos1, int pos2, List &a_list);
-template<class List_entry, class List>
+
+template<typename List_entry, class List>
 void reverse_traverse_list(List &a_list, void (*visit)(List_entry &));
-template<class List>
+
+template<typename List_entry, class List>
 Error_code copy(List &dest, List &source);
-template<class List>
+
+template<typename List_entry, class List>
 Error_code join(List &list1, List &list2);
-template<class List>
+
+template<typename List_entry, class List>
 void reverse(List &a_list);
-template<class List>
+
+template<typename List_entry, class List>
 Error_code split(List &source, List &oddlist, List &evenlist);
 
 
@@ -52,14 +62,14 @@ protected:
 };
 
 
-template<class List_entry>
+template<typename List_entry>
 List<List_entry>::List(){
     count = 0;
     for (int i = 0; i<max_list; i++)
         entry[i] = NULL;
 }
 
-template <class List_entry>
+template <typename List_entry>
 int List<List_entry>::size() const
 /*
 Post: The function returns the number of entries in the List.
@@ -68,24 +78,24 @@ Post: The function returns the number of entries in the List.
     return count;
 }
 
-template <class List_entry>
+template <typename List_entry>
 bool List<List_entry>::full() const{
     return count == max_list;
 }
 
-template<class List_entry>
+template<typename List_entry>
 bool List<List_entry>::empty()const {
     return count == 0;
 }
 
-template<class List_entry>
+template<typename List_entry>
 void List<List_entry>::clear() {
     for (int i = 0; i<max_list; i++)
         entry[i] = NULL;
-        count = 0;
+    count = 0;
 }
 
-template<class List_entry>
+template<typename List_entry>
 Error_code List<List_entry>::retrieve(int position, List_entry &x) const {
     if (position < 0 || position > count)
         return utility_range_error;
@@ -93,7 +103,7 @@ Error_code List<List_entry>::retrieve(int position, List_entry &x) const {
     return success;
 }
 
-template<class List_entry>
+template<typename List_entry>
 Error_code List<List_entry>::replace(int position, const List_entry &x){
     if (position < 0 || position > count)
         return utility_range_error;
@@ -101,7 +111,7 @@ Error_code List<List_entry>::replace(int position, const List_entry &x){
     return success;
 }
 
-template<class List_entry>
+template<typename List_entry>
 Error_code List<List_entry>::remove(int position, List_entry &x){
     if (empty())
         return underflow;
@@ -117,7 +127,7 @@ Error_code List<List_entry>::remove(int position, List_entry &x){
     return success;
 }
 
-template <class List_entry>
+template <typename List_entry>
 Error_code List<List_entry>::insert(int position, const List_entry &x)
 /*
 Post: If the List is not full and 0 <= position <= n,
@@ -140,7 +150,7 @@ Post: If the List is not full and 0 <= position <= n,
     count++;
     return success;
 }
-template <class List_entry>
+template <typename List_entry>
 void List<List_entry>::traverse(void (*visit)(List_entry &))
 /*
 Post: The action specified by function (*visit) has been performed on every
@@ -154,40 +164,40 @@ Post: The action specified by function (*visit) has been performed on every
 // Task 1 methods
 
 //E1
-template<class List_entry, class List>
+template<typename List_entry, class List>
 Error_code insert_first(const List_entry &x, List &a_list){
     if (a_list.full()) return overflow;
-    a_list.entry[0] = x;
+    a_list.insert(0, x);
     return success;
 }
 
 //E2
-template<class List_entry, class List>
+template<typename List_entry, class List>
 Error_code remove_first(List_entry &x, List &a_list){
 
     a_list.remove(0 , x);
     return success;
 }
 //E3
-template<class List_entry, class List>
+template<typename List_entry, class List>
 Error_code insert_last(const List_entry &x, List &a_list){
 
     if(a_list.full()) return overflow;
-    a_list.entry[a_list.count+1] = x;
+    a_list.insert(a_list.size(), x);
     return success;
 
 }
 
 //E4
-template<class List_entry, class List>
+template<typename List_entry, class List>
 Error_code remove_last(List_entry &x, List &a_list){
-   
+
     a_list.remove(a_list.size(), x);
     return success;
 }
 
 //E5
-template<class List_entry,class List>
+template<typename List_entry,class List>
 Error_code median_list(List_entry &x, List &a_list) {
     int median = 0;
     if (a_list.empty()) return underflow;
@@ -197,73 +207,77 @@ Error_code median_list(List_entry &x, List &a_list) {
     else{
         median = a_list.size() / 2;
     }
-    x = a_list.entry[median];
+    a_list.retrieve(median, x);
     return success;
 }
 
 //E6
-template<class List, class List_entry>
+template<typename List_entry, class List>
 Error_code interchange(int pos1, int pos2, List &a_list){
-    if (a_list.count < 2) return utility_range_error;
-    List_entry temp;
-    temp = a_list.entry[pos1];
-    a_list.entry[pos1] = a_list.entry[pos2];
-    a_list.entry[pos2] = temp;
+    List_entry pos1_val, pos2_val;
+    if (a_list.size() < 2) return utility_range_error;
+    a_list.retrieve(pos1, pos1_val);
+    a_list.retrieve(pos2, pos2_val);
+    a_list.replace(pos1, pos2_val);
+    a_list.replace(pos2, pos1_val);
     return success;
 }
 
 //E7
-template<class List_entry, class List>
+template<typename List_entry, class List>
 void reverse_traverse_list(List &a_list, void (*visit)(List_entry &)){
     //code...
 
 }
 
-template<class List>
+template<typename List_entry, class List>
 Error_code copy(List &dest, List &source){
     if (dest.empty() || source.empty()) return underflow;
-    for (int i = 0; i<dest.count; i++){
-        source.entry[i] = dest.entry[i];
+    List_entry source_val;
+    for (int i = 0; i<dest.size(); i++){
+        source.retrieve(i,source_val);
+        dest.insert(i, source_val);
     }
     return success;
 }
 
 //E8
-template<class List>
+template<typename List_entry, class List>
 Error_code join(List &dest, List &source){
     int j = 0;
-    if ((max_list - dest.count) < source.count) return overflow;
-    for (int i = dest.count+1; i <=(dest.count+1+(source.count));i++){
-        dest.entry[i] = source.entry[j];
+    List_entry temp;
+    if (max_list - dest.size() < source.size()) return overflow;
+    for (int i = dest.size()+1; i <=(dest.size()+1+(source.size()));i++){
+        dest.insert(i,source.retrieve(j, temp));
         j++;
     }
-    dest.count += source.count;
     return success;
 }
 
-template<class List, class List_entry>
+template<typename List_entry, class List>
 void reverse(List &a_list){
-    List_entry temp;
-    for (int i = 0; i<a_list.count; i++){
-        temp = a_list.entry[i];
-        a_list.entry[i] = a_list.entry[i+1];
-        a_list.entry[i+1] = temp;
+    List_entry temp,value;
+    for (int i = 0; i<a_list.size(); i++){
+        a_list.retrieve(i, temp);
+        a_list.retrieve(i+1, value);
+        a_list.replace(i, value);
+        a_list.replace(i+1,temp);
     }
 }
 
-template<class List>
+template<typename List_entry, class List>
 Error_code split(List &source, List &oddlist, List &evenlist){
     if (source.empty()) return underflow;
-
-    for (int i = 0; i<source.count; i++){
-        if (source.entry[i] % 2 == 0){
-            evenlist.insert(source.entry[i]);
-            evenlist.count++;
+    List_entry x;
+    int temp = 0;
+    for (int i = 0; i<source.size(); i++){
+        if (temp % 2 == 0){
+            evenlist.insert(i, source.retrieve(i, x ));
         }
         else {
-            oddlist.insert(source.entry[i]);
-            oddlist.count++;
+            oddlist.insert(i, source.retrieve(i,x));
         }
+        temp++;
     }
 }
 #endif
--- Week 12/main.cpp
+++ Week 12/main.cpp
@@ -6,7 +6,8 @@ int main(){
     char albphabet;
 
     List<int> numberTest;
-    List<char> alphabetTest;
+    List<int> number2Test;
+    List<int> oddlist, evenlist;
 
     numberTest.insert(0, 12);
     std::cout << "Size is: " << numberTest.size() << std::endl;
@@ -18,19 +19,31 @@ int main(){
     std::cout << "Size is now: " << numberTest.size()  << std::endl;
     numberTest.insert(0,2);
     numberTest.insert(1,4);
-    std::cout << numberTest.size() << std::endl;
+    std::cout << "After two inserts, size: " << numberTest.size() << std::endl;
     numberTest.retrieve(0,number);
-    std::cout << number << std::endl;
     remove_first(number, numberTest);
     numberTest.retrieve(0, number2);
-    std::cout << "pos0: " << number << "pos0 after remove: " << number2 << std::endl;
+    std::cout << "pos0: " << number << " ,pos0 after remove: " << number2 << std::endl;
     numberTest.clear();
-    std::cout << numberTest.size() << std::endl;
+    std::cout << "After clear, size: " << numberTest.size() << std::endl;
     numberTest.insert(0,1);
     numberTest.insert(1,2);
     numberTest.insert(2,3);
     numberTest.insert(3,4);
+    numberTest.insert(4,5);
     median_list(number, numberTest);
     std::cout << "Median: "  << number << std::endl;
+    insert_first(1, numberTest);
+    numberTest.retrieve(0, number);
+    std::cout << "Insert 1 to first place, let's see is it: " << number << std::endl;
+    insert_last(12, numberTest);
+    numberTest.retrieve(6, number);
+    std::cout << "Insert 12 to last place, let's see is it: " << number << std::endl;
+    interchange<int>(0,1, numberTest);
+    copy<int>(number2Test, numberTest);
+    join<int>(numberTest, number2Test);
+    reverse<int>(number2Test);
+    split<int>(numberTest, oddlist, evenlist);
+
     return 0;
 }
